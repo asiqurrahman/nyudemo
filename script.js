4a0e59155bccb709e0b5100c2c8f01c2dc8e5f81
Asiqur Rahman <asiqurrahman13@gmail.com>
2024-09-23 12:06:55 -0400

    our first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateText() {
+  const searchVal = document.getElementById("get-value").value;
+  const insertion = document.getElementById("insert");
+
+  insertion.innerText = searchVal;
+}
